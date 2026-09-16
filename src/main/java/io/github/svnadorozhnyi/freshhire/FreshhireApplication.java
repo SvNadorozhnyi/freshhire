29ccacbe518b36ad91548a2c54c638e986543db0
Sviatoslav Nadorozhnyi <svyatoslavnadorozhniy@gmail.com>
2026-09-16 18:19:28 -0400

    feat: initialize Spring Boot project

---
+++ src/main/java/io/github/svnadorozhnyi/freshhire/FreshhireApplication.java
@@ -0,0 +1,13 @@
+package io.github.svnadorozhnyi.freshhire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshhireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshhireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/svnadorozhnyi/freshhire/FreshhireApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.svnadorozhnyi.freshhire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshhireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
